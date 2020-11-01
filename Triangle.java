621b5dbb08fb646307dc025518c5cbb125cda03a
Yuqing Wu <ywu20@stuy.edu>
2020-11-01 09:39:19 -0500

    toString

--- Triangle.java
+++ Triangle.java
@@ -39,10 +39,10 @@ public class Triangle{
       return "scalene";
     }
   }
-/*
-  public String toString(){
 
+  public String toString(){
+    return("v1("+v1.getX()+","+v1.getY()+") v2("+v2.getX()+","+v2.getY()+") v3("+v3.getX()+","+v3.getY()+")");
   }
-  */
+
 
 }
