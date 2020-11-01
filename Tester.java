701ae03200cb427fc92e58199573977a44e2f958
Yuqing Wu <ywu20@stuy.edu>
2020-11-01 09:22:34 -0500

    triangle constructors

--- Tester.java
+++ Tester.java
@@ -3,7 +3,7 @@ public class Tester{
     Point[] points = new Point[5];
 
     //construct points
-    System.out.println("points being tested");
+    System.out.println("points being tested:");
     for(int i=0; i<4;i++){
       points[i]= new Point(Math.random()*10, Math.random()*5);
       System.out.println(points[i]);
@@ -25,6 +25,20 @@ public class Tester{
       }
     }
 
-    
+    //Triangle constructor test
+    System.out.println("triangles being tested:");
+    Triangle[] triangles = new Triangle[5];
+    for (int i=0; i<2;i++){
+      triangles[i] = new Triangle(points[i],points[i+1], points[i+2]);
+      System.out.println(triangles[i]);
+    }
+    for(int i=2; i<5;i++){
+      triangles[i]=new Triangle(Math.random()*10, Math.random()*5, Math.random()*2, Math.random()*-2, Math.random()*-4, Math.random()*3);
+      System.out.println(triangles[i]);
+    }
+
+    //
+
+
   }
 }
---
+++ Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle{
+  private Point v1;
+  private Point v2;
+  private Point v3;
+
+  public Triangle(Point a, Point b, Point c){
+    v1=a;
+    v2=b;
+    v3=c;
+  }
+
+  public Triangle(double x1, double y1,double x2, double y2,double x3, double y3){
+    v1 = new Point(x1,y1);
+    v2 = new Point(x2,y2);
+    v3 = new Point(x3,y3);
+  }
+  
+}
