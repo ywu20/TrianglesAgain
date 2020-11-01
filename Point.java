0e49e698c00e0d188d29c1182bfc487d935f1a5f
Yuqing Wu <ywu20@stuy.edu>
2020-11-01 09:07:50 -0500

    distanceTo()

--- Point.java
+++ Point.java
@@ -21,4 +21,7 @@ public class Point{
    return y;
  }
 
+public double distanceTo(Point p){
+  return Math.sqrt(Math.pow((this.x-p.x),2)+Math.pow((this.y-p.y),2));
+}
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,20 @@
 public class Tester{
   public static void main(String[] args){
-    
+    Point[] points = new Point[5];
+
+    //construct points
+    System.out.println("points being tested");
+    for(int i=0; i<4;i++){
+      points[i]= new Point(Math.random()*10, Math.random()*5);
+      System.out.println(points[i]);
+    }
+    points[4]=points[3];
+
+    //distanceTo() tester
+    for(int i=0; i<4;i++){
+      System.out.println("the distance between point "+i+" and point "+(i+1)+" is "+points[i].distanceTo(points[i+1]));
+    }
+
+    //
   }
 }
